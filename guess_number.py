7234e5c9c1cf2ab54c152f63cdd582190a936fc2
Kuwushal <kuwushal1928@gmail.com>
2024-05-27 13:16:13 +0545

    Your commit message

--- guess_number.py
+++ guess_number.py
@@ -1,6 +1,9 @@
-number = 2
+import random
 
-guess = ""
+number = random.randrange(0, 9)
+guess = None
+
+
 
 guess_limit = 3
 
@@ -9,20 +12,19 @@ out_of_guesses = False
 
 
 
-while guess!= number and not(out_of_guesses):
-    if number_of_guesses<guess_limit:
-       guess = int(input("Guess the number: "))
-       number_of_guesses += 1 
+while guess != number and not out_of_guesses:
+    if number_of_guesses < guess_limit:
+        try:
+            guess = int(input("Guess the number from 0 to 9: "))
+            number_of_guesses += 1
+        except ValueError:
+            print("Please enter a valid integer.")
     else:
         out_of_guesses = True
 
-if out_of_guesses:
+if out_of_guesses and guess != number:
     print("You Lose!")
-
 else:
     print("You Win!")
 
-    
-
-
-
+print("The number was " + str(number))
